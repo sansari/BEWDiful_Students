2137b4c0276f4648d1ed1056d342e6c022a97c2e
Brooks Swinnerton <bswinnerton@gmail.com>
2013-06-18 15:06:39 -0400

    Lesson 3 answers

--- 03_Collection_Loops/exercises/ex_loop.rb
+++ 03_Collection_Loops/exercises/ex_loop.rb
@@ -10,3 +10,13 @@
 # 1 bottle of beer!
 # You take one down and pass it around,
 # No more bottles of beer on the wall :-(
+
+99.downto(1) do |i|
+  puts "#{i} #{i.between?(2,99) ? "bottles" : "bottle"} of beer on the wall"
+  puts "#{i} #{i.between?(2,99) ? "bottles" : "bottle"} bottles of beer!"
+  puts "You take one down and pass it around,"
+  puts "#{i} #{i.between?(2,99) ? "bottles" : "bottle"} bottles of beer on the wall!"
+  puts "\n"
+end
+
+puts "No more bottles of beer on the wall :-("
